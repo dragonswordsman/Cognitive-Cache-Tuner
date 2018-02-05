7d14230dacdcef0517e784ff29a50473a86db31a
DIEGOJIM13 <diegojimenez@email.arizona.edu>
2018-02-05 12:27:31 -0700

    Fixed the formatting

--- scripts/runSimpoints_sg.pl
+++ scripts/runSimpoints_sg.pl
@@ -40,60 +40,58 @@ $interval = 100000000;
 #$BANK_SIZE = 1024;
 
 
-
-
 for($i = 0; $i < @benchmarks; $i++) {
 
-					#NOTE: In config file, clock is in ticks. 1ns = 1000 ticks. Clock period = 1/freq
-					$cacheLineSize = $cacheline_size;
-					
-					$clock0 = $clock/1000 . "GHz";
-					$l1isize0 = $l1i_size/1024 . "kB";
-					$l1iassoc0 = $l1i_assoc;
-					$l1dsize0 = $l1d_size/1024 . "kB";
-					$l1dassoc0 = $l1d_assoc;
-					
-					$l2size = $l2_size/1024 . "MB";
-					$l2assoc = $l2_assoc;
-					
+	#NOTE: In config file, clock is in ticks. 1ns = 1000 ticks. Clock period = 1/freq
+	$cacheLineSize = $cacheline_size;
 
-					#CPU 1
-					$clock1 = $clock0;
-					$l1isize1 = $l1isize0;
-					$l1iassoc1 = $l1iassoc0;
-					$l1dsize1 = $l1dsize0;
-					$l1dassoc1 = $l1dassoc0;
-					
-				#CREATE NAMES
-					$l1i_name = $l1i_size/1024;
-					$l1d_name = $l1d_size/1024;
-					
-					$l1i_cfg = $l1i_name . "k" . $l1i_assoc . "w" . $cacheline_size;
-					$l1d_cfg = $l1d_name . "k" . $l1d_assoc . "w" . $cacheline_size;
-					
-					$fastForward_name = "FF" . $fastForward / 1000000 . "m";
-					$maxInsts_name = "Max" . $maxInsts / 1000000000 . "t";
-					$interval_name = "Int" . $interval / 1000000 . "m";
-					
-					$simp_cfg = $fastForward_name . "-" . $maxInsts_name . "-" . $interval_name;
-					
-					#create simulation directory
-					if(!(-d "$stats/$benchmarks[$i]"))
-						{
-							system("/bin/mkdir $stats/$benchmarks[$i]");
-						}
-					#$sim_dir_name = "$stats/$benchmarks[$i]";
-					$sim_dir_name = "$stats/$benchmarks[$i]/$simp_cfg";		#/$l1i_cfg-$l1d_cfg-$clock0";
-					if(!(-d "$sim_dir_name"))
-					{
-						system("/bin/mkdir $sim_dir_name");
-					}
-					$simpoint_dir = "$sim_dir_name/simpoints";
-					if(!(-d "$simpoint_dir"))
-					{	
-						system("/bin/mkdir $simpoint_dir");# or die "Cannot create directory $!";
-					}
-					
+	$clock0 = $clock/1000 . "GHz";
+	$l1isize0 = $l1i_size/1024 . "kB";
+	$l1iassoc0 = $l1i_assoc;
+	$l1dsize0 = $l1d_size/1024 . "kB";
+	$l1dassoc0 = $l1d_assoc;
+
+	$l2size = $l2_size/1024 . "MB";
+	$l2assoc = $l2_assoc;
+
+
+	#CPU 1
+	$clock1 = $clock0;
+	$l1isize1 = $l1isize0;
+	$l1iassoc1 = $l1iassoc0;
+	$l1dsize1 = $l1dsize0;
+	$l1dassoc1 = $l1dassoc0;
+
+	#CREATE NAMES
+	$l1i_name = $l1i_size/1024;
+	$l1d_name = $l1d_size/1024;
+
+	$l1i_cfg = $l1i_name . "k" . $l1i_assoc . "w" . $cacheline_size;
+	$l1d_cfg = $l1d_name . "k" . $l1d_assoc . "w" . $cacheline_size;
+
+	$fastForward_name = "FF" . $fastForward / 1000000 . "m";
+	$maxInsts_name = "Max" . $maxInsts / 1000000000 . "t";
+	$interval_name = "Int" . $interval / 1000000 . "m";
+
+	$simp_cfg = $fastForward_name . "-" . $maxInsts_name . "-" . $interval_name;
+
+	#create simulation directory
+	if(!(-d "$stats/$benchmarks[$i]"))
+		{
+			system("/bin/mkdir $stats/$benchmarks[$i]");
+		}
+	#$sim_dir_name = "$stats/$benchmarks[$i]";
+	$sim_dir_name = "$stats/$benchmarks[$i]/$simp_cfg";		#/$l1i_cfg-$l1d_cfg-$clock0";
+	if(!(-d "$sim_dir_name"))
+	{
+		system("/bin/mkdir $sim_dir_name");
+	}
+	$simpoint_dir = "$sim_dir_name/simpoints";
+	if(!(-d "$simpoint_dir"))
+	{	
+		system("/bin/mkdir $simpoint_dir");# or die "Cannot create directory $!";
+	}
+
 
 
 $statsFileName = "$sim_dir_name/$benchmarks[$i].txt";
--- scripts/runSimulations_sg.pl
+++ scripts/runSimulations_sg.pl
@@ -48,50 +48,50 @@ $interval = 1000000;
 
 #for($i = 2; $i < 4; $i++) {
 
-					#NOTE: In config file, clock is in ticks. 1ns = 1000 ticks. Clock period = 1/freq
-					$cacheLineSize = $cacheline_size;
-					
-					$clock0 = $clock/1000 . "GHz";
-					$l1isize0 = $l1i_size/1024 . "kB";
-					$l1iassoc0 = $l1i_assoc;
-					$l1dsize0 = $l1d_size/1024 . "kB";
-					$l1dassoc0 = $l1d_assoc;
-					
-					$l2size = $l2_size/1024 . "MB";
-					$l2assoc = $l2_assoc;
-					
+#NOTE: In config file, clock is in ticks. 1ns = 1000 ticks. Clock period = 1/freq
+$cacheLineSize = $cacheline_size;
 
-					#CPU 1
-					$clock1 = $clock0;
-					$l1isize1 = $l1isize0;
-					$l1iassoc1 = $l1iassoc0;
-					$l1dsize1 = $l1dsize0;
-					$l1dassoc1 = $l1dassoc0;
-					
-				#CREATE NAMES
-					$l1i_name = $l1i_size/1024;
-					$l1d_name = $l1d_size/1024;
-					
-					$l1i_cfg = $l1i_name . "k" . $l1i_assoc . "w" . $cacheline_size;
-					$l1d_cfg = $l1d_name . "k" . $l1d_assoc . "w" . $cacheline_size;
-					
-					$fastForward_name = "FF" . $fastForward / 1000000 . "m";
-					$maxInsts_name = "Max" . $maxInsts / 1000000000 . "t";
-					$interval_name = "Int" . $interval / 1000000 . "m";
-					
-					$simp_cfg = $fastForward_name . "-" . $maxInsts_name . "-" . $interval_name;
-					
-					#create simulation directory
-					if(!(-d "$stats/$benchmarks[$i]"))
-						{
-							system("/bin/mkdir $stats/$benchmarks[$i]");
-						}
-					$config_name = "$l1i_cfg-$l1d_cfg-$clock0";
-					$sim_dir_name = "$stats/$benchmarks[$i]/$l1i_cfg-$l1d_cfg-$clock0";
-					if(!(-d "$sim_dir_name"))
-					{
-						system("/bin/mkdir $sim_dir_name");
-					}
+$clock0 = $clock/1000 . "GHz";
+$l1isize0 = $l1i_size/1024 . "kB";
+$l1iassoc0 = $l1i_assoc;
+$l1dsize0 = $l1d_size/1024 . "kB";
+$l1dassoc0 = $l1d_assoc;
+
+$l2size = $l2_size/1024 . "MB";
+$l2assoc = $l2_assoc;
+
+
+#CPU 1
+$clock1 = $clock0;
+$l1isize1 = $l1isize0;
+$l1iassoc1 = $l1iassoc0;
+$l1dsize1 = $l1dsize0;
+$l1dassoc1 = $l1dassoc0;
+
+#CREATE NAMES
+$l1i_name = $l1i_size/1024;
+$l1d_name = $l1d_size/1024;
+
+$l1i_cfg = $l1i_name . "k" . $l1i_assoc . "w" . $cacheline_size;
+$l1d_cfg = $l1d_name . "k" . $l1d_assoc . "w" . $cacheline_size;
+
+$fastForward_name = "FF" . $fastForward / 1000000 . "m";
+$maxInsts_name = "Max" . $maxInsts / 1000000000 . "t";
+$interval_name = "Int" . $interval / 1000000 . "m";
+
+$simp_cfg = $fastForward_name . "-" . $maxInsts_name . "-" . $interval_name;
+
+#create simulation directory
+if(!(-d "$stats/$benchmarks[$i]"))
+	{
+		system("/bin/mkdir $stats/$benchmarks[$i]");
+	}
+$config_name = "$l1i_cfg-$l1d_cfg-$clock0";
+$sim_dir_name = "$stats/$benchmarks[$i]/$l1i_cfg-$l1d_cfg-$clock0";
+if(!(-d "$sim_dir_name"))
+{
+	system("/bin/mkdir $sim_dir_name");
+}
 					
 
 
